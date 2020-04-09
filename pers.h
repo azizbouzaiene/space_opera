4b75c0dd135e68d62197f75412f43bb2a87bb1b3
wissalsoudani <61351743+wissalsoudani@users.noreply.github.com>
2020-04-09 14:56:08 +0100

    Update pers.h

--- pers.h
+++ pers.h
@@ -1,14 +1,32 @@
+#ifndef HERO_H_INCLUDED
+#define HERO_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
 #include "scoreVie.h"
+
+typedef struct 
+{
+ int jump,walk,hit,kick,die,punch;
+}movement;
+
 typedef struct 
 {
 SDL_Surface* pers[20];
+SDL_Surface *herosprite;
+ SDL_Rect  heroposrect;
 int nbrIm;
 int numIm;
 int direction;
-SDL_Rect pos;
+int heropos
+struct movement
 char name[20];
 score s;
 vie v;
 }personnage;
+
 personnage initialiser_personnage();
 void  afficher_personnage(personnage p,SDL-Surface *ecran);
